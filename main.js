594b0eff67ed250cc370c7580211e6838a3e0333
lehn <laurence.for5@gmail.com>
2024-02-20 15:29:09 +0900

    first---commit

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 let news = [];
 const getLatestNews = async () => {
-  const url = new URL(`https://tubular-crostata-0ed0ae.netlify.app`);
+  const url = new URL(
+    `https://tubular-crostata-0ed0ae.netlify.app/top-headlines`
+  );
   const response = await fetch(url);
   const data = await response.json();
   news = data.articles;
