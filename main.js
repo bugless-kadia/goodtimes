b4c4d33906ed1a4c47a8d095b8c6822bd74e51f7
lehn <laurence.for5@gmail.com>
2024-02-21 23:56:29 +0900

    second commit

--- main.js
+++ main.js
@@ -1,11 +1,65 @@
-let news = [];
+let newsList = [];
 const getLatestNews = async () => {
   const url = new URL(
     `https://tubular-crostata-0ed0ae.netlify.app/top-headlines`
   );
   const response = await fetch(url);
   const data = await response.json();
-  news = data.articles;
-  console.log('rrr', news);
+  newsList = data.articles;
+  render();
+  console.log('rrr', newsList);
+};
+
+const openNav = () => {
+  document.getElementById('mySidenav').style.width = '250px';
+};
+const closeNav = () => {
+  document.getElementById('mySidenav').style.width = '0';
+};
+
+const openSearchBox = () => {
+  let inputArea = document.getElementById('input-area');
+  if (inputArea.style.display === 'inline') {
+    inputArea.style.display = 'none';
+  } else {
+    inputArea.style.display = 'inline';
+  }
+};
+
+const render = () => {
+  const newsHTML = newsList
+    .map(
+      (news) =>
+        `<div class="row news">
+    <div class="col-lg-4">
+      <img
+        class="news-img-size"
+        src=${
+          news.urlToImage ||
+          'https://st4.depositphotos.com/14953852/22772/v/450/depositphotos_227724992-stock-illustration-image-available-icon-flat-vector.jpg'
+        }
+      />
+    </div>
+    <div class="col-lg-8">
+      <h2>${news.title}</h2>
+      <p>
+        ${
+          news.description == null || news.description == ''
+            ? '내용없음'
+            : news.description.length > 200
+            ? news.description.substring(0, 200) + '...'
+            : news.description
+        }
+      </p>
+      <div>
+        ${news.source.name || 'no source'} * ${moment(
+          news.publishedAt
+        ).fromNow()}
+      </div>
+    </div>
+  </div>`
+    )
+    .join('');
+  document.getElementById('news-board').innerHTML = newsHTML;
 };
 getLatestNews();
